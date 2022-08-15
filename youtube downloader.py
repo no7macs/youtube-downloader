030dcc14144034a0f83d97124c170442d96efc27
no7mac <44577325+no7macs@users.noreply.github.com>
2022-08-14 20:35:31 -0600

    now retries download if failed + removed ffmpeg usage for the time

--- youtube downloader.py
+++ youtube downloader.py
@@ -26,11 +26,16 @@ def execution(sema, folder, link):
                 'format': 'bestvideo+bestaudio',
                 'outtmpl':f"""{folder}/%(title)s-%(id)s.%(ext)s""",
                 'download_archive':f'./{folder}/archive.txt',
+                'cachedir':f"""./{folder}/cache""",
                 'cookiefile':'./youtube.com_cookies.txt',
                 'merge_output_format':'mkv'
-                }       
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([link])
+                }
+    try:       
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+    except:
+        execution(sema, folder, link)
+
 
     # download all videos
     #--all-subs --write-description --write-info-json --write-annotations --all-subs --write-all-thumbnails
@@ -42,30 +47,6 @@ def execution(sema, folder, link):
     gc.collect()
 
     sema.release()
-
-    # make webm
-    files = os.listdir(f"""./{folder}/""")
-    for a in files:
-        if os.path.splitext(a)[1] == '.mkv':
-            result = subprocess.check_output(
-                    f'ffprobe -v quiet -show_streams -select_streams v:0 -of json "./{folder}/{a}"',
-                    shell=True).decode()
-            fields = json.loads(result)['streams'][0]
-            duration = fields['tags']['DURATION']
-            fps = fields['r_frame_rate']
-            date_time_obj = datetime.datetime.strptime(duration.split('.')[0], '%X')
-            print(date_time_obj.second)
-            if date_time_obj.minute < 2:
-                full_file_path = f"""./{folder}/{a}"""
-                #{(os.path.getsize(full_file_path)*1024)/date_time_obj.second}
-                try:
-                    #webm
-                    os.system(f"""ffmpeg -i "{full_file_path}" -c:v libvpx -b:v {(5243000)/date_time_obj.second} -pass 1 -an -f -n  "./{folder}/temp/" """)
-                    os.system(f"""ffmpeg -i "{full_file_path}" -c:v libvpx -b:v {(5243000)/date_time_obj.second} -preset veryslow -pass 2 -c:a libopus -n "./{folder}/webm/{os.path.splitext(a)[0]}.webm" """)
-                    #mp4
-                except Exception as error:
-                    print("ffmpeg webm encode error \n" + str(error))
-                    os.remove(full_file_path)
     gc.collect()
 
     # remove big webm
